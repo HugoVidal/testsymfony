6f73553bb18ae7a73d5ea21ccd0b6f7e1626d5c6
hugovidal <hvidal@viaxoft.com>
2020-02-29 17:52:27 +0100

    Bootstrap and css integration

--- test_symfony4_2/src/Controller/HomeController.php
+++ test_symfony4_2/src/Controller/HomeController.php
@@ -1,45 +1,45 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
-use App\Entity\Question;
-use App\Form\QuestionType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\Question;
+    use App\Form\QuestionType;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController {
+    class HomeController extends AbstractController {
 
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
+        /**
+         * @var EntityManagerInterface
+         */
+        private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager) {
-        $this->entityManager = $entityManager;
-    }
-
-    /**
-     * @Route("/", name="home")
-     * @param Request $request
-     * @return Response
-     */
-    public function index(Request $request): Response {
-        $question = new Question();
-        $form = $this->createForm(QuestionType::class, $question);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted()) {
-            $this->entityManager->persist($question);
-            $this->entityManager->flush();
-            return $this->redirectToRoute('home');
+        public function __construct(EntityManagerInterface $entityManager) {
+            $this->entityManager = $entityManager;
         }
 
-        return $this->render('pages/home.html.twig', [
-            'question' => $question,
-            'form' => $form->createView()
-        ]);
+        /**
+         * @Route("/", name="home")
+         * @param Request $request
+         * @return Response
+         */
+        public function index(Request $request): Response {
+            $question = new Question();
+            $form = $this->createForm(QuestionType::class, $question);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted()) {
+                $this->entityManager->persist($question);
+                $this->entityManager->flush();
+                return $this->redirectToRoute('home');
+            }
+
+            return $this->render('pages/home.html.twig', [
+                'question' => $question,
+                'form' => $form->createView()
+            ]);
+        }
     }
-}
--- test_symfony4_2/src/Controller/QuestionController.php
+++ test_symfony4_2/src/Controller/QuestionController.php
@@ -1,29 +1,29 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
-use App\Repository\QuestionRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Repository\QuestionRepository;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController extends AbstractController {
+    class QuestionController extends AbstractController {
 
-    /**
-     * @var QuestionRepository
-     */
-    private $repository;
+        /**
+         * @var QuestionRepository
+         */
+        private $repository;
 
-    public function __construct(QuestionRepository $repository) {
-        $this->repository = $repository;
+        public function __construct(QuestionRepository $repository) {
+            $this->repository = $repository;
+        }
+
+        /**
+         * @Route("/questions", name="questions")
+         * @return Response
+         */
+        public function index(): Response {
+            $questions = $this->repository->findAll();
+            return $this->render('pages/question.html.twig', ['questions' => $questions]);
+        }
     }
-
-    /**
-     * @Route("/questions", name="questions")
-     * @return Response
-     */
-    public function index(): Response {
-        $questions = $this->repository->findAll();
-        return $this->render('pages/question.html.twig', ['questions' => $questions]);
-    }
-}
